474bdda62ee3b34496bf1218d57a2c3dd9895dd1
fishmeisterFTW <63541672+fishmeister09@users.noreply.github.com>
2021-06-05 14:58:31 +0530

    added fundraised

--- src/models/RequestForm.js
+++ src/models/RequestForm.js
@@ -5,10 +5,11 @@ const Schema = mongoose.Schema;
 const requestSchema = new Schema(
   {
     username: {type: String, required: true},
-    email: {type: String, required: true},
     title: {type: String, required: true},
+    email: {type: String, required: true},
     description: {type: String, required: true},
     fundrequired: {type: Number, required: true},
+    fundraised: {type: Number, required: true},
     upi: {type: String, required: true},
   },
   {
