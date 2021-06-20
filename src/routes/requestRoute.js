0ad11dfa3bc50e30db6383b612683181a1b49d65
fishmeisterFTW <63541672+fishmeister09@users.noreply.github.com>
2021-06-20 20:33:25 +0530

    Update requestRoute.js

--- src/routes/requestRoute.js
+++ src/routes/requestRoute.js
@@ -8,6 +8,19 @@ router.route('/displayreq').get((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/displayreq/:id').post((req, res) => {
+  Request.findById(req.params.id)
+    .then(request => {
+      request.fundraised = Number(req.body.fundraised);
+
+      request
+        .save()
+        .then(() => res.json('fundraised updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
 router.route('/request').post((req, res) => {
   const username = req.body.username;
   const title = req.body.title;
